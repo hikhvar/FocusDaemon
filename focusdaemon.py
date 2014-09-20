c7e1bb37aea1a3ec8a4fb958ba7e59cbea54b985
Christoph Robbert <crobbert@mail.uni-paderborn.de>
2014-09-20 17:48:57 +0200

    Some bugfixes. Get still the 'glib.GError: GDBus.Error:org.freedesktop.DBus.Error.ServiceUnknown: The name :1.201 was not provided by any .service files ' error after 25 Minutes of work time on Ubuntu 12.04

--- focusdaemon.py
+++ focusdaemon.py
@@ -5,6 +5,7 @@ import pynotify
 import sys
 import argparse
 import time
+import datetime
 
 def parse_arguements():
     """
@@ -27,23 +28,41 @@ def countdown(seconds=60):
     n = None
     for i in xrange(seconds):
         if n is None:
-            n = pynotify.Notification(headline, text % (seconds -i))
+            n = send_notification(headline, text % (seconds -i))
         else:
             n.update(headline, text % (seconds -i))
-        n.show()
+            n.show()
         time.sleep(1)
 
-def loop(args):
-    n = pynotify.Notification("Worktime!", "In %s minutes begins your next freetime." % args.workingtime) 
+def send_notification(header, text):
+    """
+        sends a notification and return the object for later use (updates etc)
+    """
+    if not pynotify.init("Basics"):
+        return None
+    n = pynotify.Notification(header, text) 
     n.show()
+    print datetime.datetime.now().isoformat(), header, text
+    return n
+
+def _get_time_in(minutes, seperator=":"):
+    """
+        return the clocktime in the given minutes from now
+    """
+    now = datetime.datetime.now()
+    delta = datetime.timedelta(minutes=minutes)
+    end = now + delta
+    return seperator.join(map(lambda x: str(x).zfill(2), [end.hour, end.minute]))
+
+def loop(args):
+
+    send_notification("Worktime!", "At %s in %s minutes begins your next freetime." % (_get_time_in(args.workingtime),args.workingtime)) 
     while True:
         time.sleep(args.workingtime*60)
-        n = pynotify.Notification("Freetime!", "Freetime begins. You have %s minutes left" % args.freetime)
-        n.show()
+        send_notification("Freetime!", "Freetime begins. You have %s minutes left" % args.freetime)
         time.sleep((args.freetime-1)*60)
         countdown()
-        n = pynotify.Notification("Worktime!", "Freetime ends. In %s minutes begins your next freetime." % args.workingtime) 
-        n.show()
+        send_notification("Worktime!", "At %s in %s minutes begins your next freetime." % (_get_time_in(args.workingtime),args.workingtime)) 
 
 def main():
     if not pynotify.init("Basics"):
